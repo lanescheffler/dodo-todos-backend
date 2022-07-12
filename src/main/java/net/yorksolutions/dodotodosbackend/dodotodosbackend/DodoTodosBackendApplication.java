a73993699cd002a100c43032c2a1ab53f226eb25
yorkmacbook058 <lanescheffler@gmail.com>
2022-07-12 11:32:15 -0500

    initial commit 1.0 : set up database, docker, gitignore, & application properties

---
+++ src/main/java/net/yorksolutions/dodotodosbackend/dodotodosbackend/DodoTodosBackendApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.dodotodosbackend.dodotodosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoTodosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodoTodosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/dodotodosbackend/dodotodosbackend/DodoTodosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.dodotodosbackend.dodotodosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodoTodosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
